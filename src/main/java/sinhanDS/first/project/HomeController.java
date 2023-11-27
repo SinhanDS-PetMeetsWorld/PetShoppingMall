e9e5fde94224e4811434c7e395259efabc8b82dc
rlaqudcjs96 <rlaqudcjs96@gmail.com>
2023-11-27 19:57:04 +0900

    231

--- src/main/java/sinhanDS/first/project/HomeController.java
+++ src/main/java/sinhanDS/first/project/HomeController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	//진환의 흔적 2
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
