b754a445907853011d877c0955f5a653c0c1c8d2
jhyoo <jhyoo1224@gmail.com>
2023-12-04 21:30:24 +0900

    paymentVO 일부 수정, seller, payment 테이블 수정

--- src/main/java/sinhanDS/first/project/user/VO/PaymentVO.java
+++ src/main/java/sinhanDS/first/project/user/VO/PaymentVO.java
@@ -11,34 +11,33 @@ public class PaymentVO {
 	private int user_no;
 	private int seller_no;
 	private String name;
-	private int type1;
-	private int[] type1_list;
+	private int type;
 	private String company;
 	private String account;
 	
-	private List<ArrayList<String>> type;
+	private List<ArrayList<String>> company_list;
 	public PaymentVO(){
-		type = new ArrayList<ArrayList<String>>();
-		ArrayList<String> type0 = new ArrayList<>();
-		type0.add("신한은행");
-		type0.add("국민은행");
-		type0.add("KEB하나은행");
-		type0.add("SC제일은행");
-		type0.add("우리은행");
-		type0.add("외환은행");
-		type0.add("한국시티은행");
-		type0.add("경남은행");
-		type0.add("광주은행");
-		type0.add("대구은행");
-		type0.add("부산은행");
-		type0.add("전북은행");
-		type0.add("제주은행");
-		type0.add("기업은행");
-		type0.add("농협은행");
-		type0.add("수협은행");
-		type0.add("한국산업은행");
-		type0.add("한국수출입은행");
-		type0.add("우체국예금");
-		type.add(type0);
+		company_list = new ArrayList<ArrayList<String>>();
+		ArrayList<String> company0 = new ArrayList<>();
+		company0.add("신한은행");
+		company0.add("국민은행");
+		company0.add("KEB하나은행");
+		company0.add("SC제일은행");
+		company0.add("우리은행");
+		company0.add("외환은행");
+		company0.add("한국시티은행");
+		company0.add("경남은행");
+		company0.add("광주은행");
+		company0.add("대구은행");
+		company0.add("부산은행");
+		company0.add("전북은행");
+		company0.add("제주은행");
+		company0.add("기업은행");
+		company0.add("농협은행");
+		company0.add("수협은행");
+		company0.add("한국산업은행");
+		company0.add("한국수출입은행");
+		company0.add("우체국예금");
+		company_list.add(company0);
 	}
 }
--- src/main/webapp/WEB-INF/views/seller/login/join.jsp
+++ src/main/webapp/WEB-INF/views/seller/login/join.jsp
@@ -255,26 +255,11 @@
         
         <div>
             정산 계좌*<br>
-            <select name="" id="">
-                <option value="0">신한은행</option>
-                <option value="1">국민은행</option>
-                <option value="2">KEB하나은행</option>
-                <option value="3">SC제일은행</option>
-                <option value="4">우리은행</option>
-                <option value="5">외환은행</option>
-                <option value="6">한국시티은행</option>
-                <option value="7">경남은행</option>
-                <option value="8">광주은행</option>
-                <option value="9">대구은행</option>
-                <option value="10">부산은행</option>
-                <option value="11">전북은행</option>
-                <option value="12">제주은행</option>
-                <option value="13">기업은행</option>
-                <option value="14">농협은행</option>
-                <option value="15">수협은행</option>
-                <option value="16">한국산업은행</option>
-                <option value="17">한국수출입은행</option>
-                <option value="18">우체국예금보험</option>
+            <select name="" id="" data-no="${type.index }">
+				<c:forEach var="type" items="${vo.company[0] }"
+					varStatus="status">
+				<option value="${type.index}">${type }</option>
+				</c:forEach>
             </select>
             <input type="text" name="" id="" placeholder="'-'없이 숫자만 입력하세요">
         </div>
