329b048b32a0b8fa6f9aa7f09fe3017d077a25f1
dbwlsghks258 <step_z@naver.com>
2023-12-19 21:53:46 +0900

    1

--- src/main/java/sinhanDS/first/project/user/order/OrderController.java
+++ src/main/java/sinhanDS/first/project/user/order/OrderController.java
@@ -127,17 +127,15 @@ public class OrderController {
 		log.debug("optionList체크: " + option_list);
 		orderService.registOrderDetailOption(option_list, detailList, cart_no, option_cart_no);
 		/*주문 상세 옵션 넣어주면됨 */
-<<<<<<< HEAD
+
 		
 		
 
 		System.out.println("카트에노" + Arrays.toString(cart_no)); // 내가 체크한 상품의 cart_no 정보가 들어있음
 		
 		
-		return "user/order/success";
-=======
+
 		return "redirect:/user/order/success.do";
->>>>>>> branch 'master' of https://github.com/kimcider/PetShoppingMall
 	}
 	
 	@GetMapping("list.do")
